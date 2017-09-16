4262954c6dd84694c8ca3cf18ce0f0c448c12543
Soumya R Jagdev <shiv7686@users.noreply.github.com>
2017-09-16 08:43:06 -0400

    Add files via upload

--- meow.js
+++ meow.js
@@ -1,15 +1,15 @@
 var provider = new firebase.auth.GoogleAuthProvider();
-var user;
 
 function googleSignin() {
    firebase.auth()
    
    .signInWithPopup(provider).then(function(result) {
       var token = result.credential.accessToken;
-      user = result.user;
+      var user = result.user;
 		
       console.log("TOKEN " + token)
       console.log("USER " + user)
+      console.log(user.displayName)
    }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
