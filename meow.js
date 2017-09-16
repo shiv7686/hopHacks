e2dd4aebd04109e16547db435221799f324733ad
Soumya R Jagdev <shiv7686@users.noreply.github.com>
2017-09-16 09:04:24 -0400

    Add files via upload

--- meow.js
+++ meow.js
@@ -10,6 +10,7 @@ function googleSignin() {
 		
       console.log("TOKEN " + token)
       console.log("USER " + user)
+      console.log(user.displayName)
    }).catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -28,23 +29,6 @@ function googleSignout() {
    });
 }
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    console.log(displayName);
-    // ...
-  } else {
-    // User is signed out.
-    // ...
-  }
-});
 
 
 
