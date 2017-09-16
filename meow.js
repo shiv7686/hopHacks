1a6cf6647842a4c40c802133413ce6c011052372
Soumya R Jagdev <shiv7686@users.noreply.github.com>
2017-09-16 09:37:48 -0400

    Add files via upload

--- meow.js
+++ meow.js
@@ -1,4 +1,6 @@
 var provider = new firebase.auth.GoogleAuthProvider();
+var provider1	 = new firebase.auth.FacebookAuthProvider();
+
 var user;
 
 function googleSignin() {
@@ -29,6 +31,26 @@ function googleSignout() {
    });
 }
 
+
+function facebookLogIn()
+{
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
 
 
 
