31d361f3ae9e332a46351763cf3ff115e5176a42
Soumya R Jagdev <shiv7686@users.noreply.github.com>
2017-09-16 10:29:34 -0400

    Add files via upload

--- meow.js
+++ meow.js
@@ -6,19 +6,23 @@ var email;
 var photoURL;
 var gender;
 
+
+// Sign into goofle
 function googleSignin() {
    firebase.auth()
    
    .signInWithPopup(provider).then(function(result) {
+   	//token can be used to get more information about user
       var token = result.credential.accessToken;
       user = result.user;
-		
-      console.log("TOKEN " + token);
-      console.log("USER " + user);
+      // console.log("TOKEN " + token);
+      // console.log("USER " + user);
       displayName = (user.displayName);
       email = (user.email);
       photoURL = (user.photoURL);
       gender = (user.gender);
+      // print name to html
+      document.write("Hello " + displayName);
       
    }).catch(function(error) {
       var errorCode = error.code;
@@ -27,7 +31,7 @@ function googleSignin() {
    });
 }
 
-
+//Sign out
 function googleSignout() {
    firebase.auth().signOut()
 	
